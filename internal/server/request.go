99f2bec9bdb36e5339ea9568b3c376706291028c
meinantoyuriawan <meinantoyuriawan@gmail.com>
2025-09-05 15:27:40 +0700

    fix: request validator and error handling

--- internal/server/request.go
+++ internal/server/request.go
@@ -11,11 +11,11 @@ type RegisterRequest struct {
 }
 
 type PatchUserRequest struct {
-	Preference *string `json:"preference,omitempty"`
-	Weightunit *string `json:"weightUnit,omitempty"`
-	Heightunit *string `json:"heightUnit,omitempty"`
-	Weight     *int64  `json:"weight,omitempty"`
-	Height     *int64  `json:"height,omitempty"`
-	Name       *string `json:"name,omitempty"`
-	Imageuri   *string `json:"imageUri,omitempty"`
+	Preference *string `json:"preference,omitempty" validate:"required,oneof=CARDIO WEIGHT"`
+	Weightunit *string `json:"weightUnit,omitempty" validate:"required,oneof=KG LBS"`
+	Heightunit *string `json:"heightUnit,omitempty" validate:"required,oneof=CM INCH"`
+	Weight     *int64  `json:"weight,omitempty" validate:"required,gte=10,lte=1000"`
+	Height     *int64  `json:"height,omitempty" validate:"required,gte=3,lte=250"`
+	Name       *string `json:"name,omitempty" validate:"omitempty,min=2,max=60"`
+	Imageuri   *string `json:"imageUri,omitempty" validate:"omitempty,uri"`
 }
